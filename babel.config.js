6004d9ae68641a2cac9fc417570f03afc7ea89cf
Savan <sasodariya@eedgetechnology.com>
2024-09-13 11:16:06 +0530

    setup import alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        root: ['.'],
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Component1 from '@src/comp';
+
+function App() {
+  return <Component1 />;
+}
+
+export default App;
